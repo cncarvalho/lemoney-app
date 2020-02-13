2e8b6e529667031ba9d1decf691547a5a47e00e3
Cesar de Carvalho <cesarncarvalho@gmail.com>
2020-02-13 17:47:33 -0300

    [LEMONEY-12]
    - Creating the offer registration form React component and implementing its behavior.

---
+++ app/javascript/components/admin/offers/views/new/new.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import BaseForm from "../../base_form/base_form";
+import View from "../../../../view/view";
+
+const New = () => {
+	return (
+		<View>
+			<BaseForm/>
+		</View>
+	);
+};
+
+export default New
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
 	namespace :admin do
-		resources :offers, only: :index
+		resources :offers, only: %i[index new]
 	end
 end
